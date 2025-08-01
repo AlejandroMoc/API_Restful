a1ad0d6a2ecf8b40d0d2c3a0f255dcb75aad9473
Alejandro Moctezuma <moctezumaalejandro25@gmail.com>
2025-08-01 14:44:58 -0600

    Add base to challengeService

--- src/utils/challengeService.js
+++ src/utils/challengeService.js
@@ -5,23 +5,44 @@ const UserChallenge = require('../models/UserChallenge');
 // Calcular progreso para todos los retos
 async function calculateChallengeProgress(user) {
   try {
-    // Obtener todos los retos
+
+    // Obtener todos los Challenges
     const challenges = await Challenge.find({});
 
+    // Iterar por Challenges
     for (const challenge of challenges) {
       const challengeData = await Challenge.findById(challenge._id);
       if (!challengeData) {
         console.error('No se encontró el reto:', challenge);
-        // Ir al siguiente reto
         continue;
       }
-
-      let startDate = new Date(Date.now());
-
-      // Encontrar UserChallenge correspondiente
-      // a combinación de reto y usuario
-      let userChallenge = await UserChallenge.findOne({ user: user, challenge: challenge._id });
       
+      let startDate = new Date(Date.now());
+      // Encontrar UserChallenge correspondiente p/reto y usuario
+      let userChallenge = await UserChallenge.findOne({ user: user, challenge: challenge._id });
+
+      // Si existe UserChallenge
+      if (userChallenge) {
+
+        // Si no existe fecha de creación, añadirla como Date.now
+        if(!userChallenge.startDate){
+            userChallenge.startDate = startDate;
+            await userChallenge.save();
+        }
+
+        // Si el reto es de tipo diario,
+        // verificar si el usuario cumplió la meta diaria
+        if (challengeData.goalType === 'daily') {
+          console.log('De tipo Daily');
+        
+        // Si el reto es acumulativo, calcular porcentaje
+        } else if (challengeData.goalType === 'accumulative') {
+          console.log('De tipo acumulativo');
+        }
+
+      } else {
+        console.log('No existe UserChallenge para esa combinación.');
+      }
     }
   } catch (error) {
     console.error('Error al calcular progreso de los retos:', error);
